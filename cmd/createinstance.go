90ecf915bea141adbc86f745eecd091278c02ae7
admdwrf <frederic.alix@fredalix.com>
2016-11-17 17:37:01 +0100

    restart by zero

--- cmd/createinstance.go
+++ cmd/createinstance.go
@@ -1,23 +1,13 @@
-// Copyright © 2016 NAME HERE <EMAIL ADDRESS>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package cmd
 
 import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+)
+
+const (
+	statusTimeout = 200
 )
 
 // createinstanceCmd represents the createinstance command
@@ -38,7 +28,7 @@ to quickly create a Cobra application.`,
 
 func init() {
 	RootCmd.AddCommand(createinstanceCmd)
-
+	createinstanceCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
